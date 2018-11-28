6a460344ec0b6d141940269b1aaa257d3538d5b2
wl <w2015long@qq.com>
2018-11-28 21:19:32 +0800

    ri li list

---
+++ mi-band/js/band.js
@@ -0,0 +1,26 @@
+var aBtn = document.querySelectorAll('.header-nav-item a');
+var aList = document.querySelectorAll('.children-list');
+
+// console.log(aBtn);
+for(var i=0; i<aBtn.length;i++){
+	aBtn[i].index=i;
+	aBtn[i].onmouseover=function(){
+		for(var j=0;j<aList.length;j++){
+			aBtn[j].className='';
+			aList[j].style.display='none';
+		}
+		this.className='active';
+		aList[this.index].style.display='block';
+	}
+}
+for(var i=0; i<aBtn.length;i++){
+	aBtn[i].index=i;
+	aBtn[i].onmouseout=function(){
+		for(var j=0;j<aList.length;j++){
+			// aBtn[j].className='active';
+			aList[j].style.display='display';
+		}
+		this.className='';
+		aList[this.index].style.display='';
+	}
+}
