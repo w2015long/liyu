a62eede4e10bb4110f65cb9f46becfd476664c5b
long <1521300387@qq.com>
2018-12-15 22:46:02 +0800

    first add js for cart at mi

--- MI/js/mi.js
+++ MI/js/mi.js
@@ -2,7 +2,16 @@
 function byId(id){
     return typeof(id) === "string" ? document.getElementById(id) : id;
 }
- 
+ /*头部购物车*/
+ var oCart = document.querySelector('.cart');
+ var oCartBox = document.querySelector('.cart-box');
+ var oContent = document.querySelector('.cart-content');
+ var oCartA = oCartBox.children[0];
+ oCart.onmouseenter = function(){
+ 	oContent.style.display = 'block';
+ 	oCartBox.style.backgroundColor = '#fff';
+ 	oCartA.style.color = '#ff6700';
+ }
 /*carousel轮播图*/
 var timer=null;
 var now=0;
