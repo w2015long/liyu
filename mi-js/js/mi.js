a17abb6382fa8ca5a2226efb6c0d77ddf29daf75
long <1521300387@qq.com>
2018-12-24 23:05:21 +0800

    add [obj.data] for nav at mi

--- mi-js/js/mi.js
+++ mi-js/js/mi.js
@@ -41,30 +41,63 @@ function handleNav(){
 	var aNavLi = document.querySelectorAll('.header-nav .header-nav-item');
 	var hiddenTimer = null;
 	oNavBox.style.overflow = 'hidden';
-	for(var i=0;i<aNavLi.length;i++){
-		clearTimeout(hiddenTimer);
+	for(var i=0;i<aNavLi.length-2;i++){
+		aNavLi[i].index = i;
 		aNavLi[i].onmouseenter = function(){
-			// oNavBox.style.overflow = 'hidden';
+			//移入到其他Li时也清除定时器
+			clearTimeout(hiddenTimer);
 			oNavBox.style.borderTop = '1px solid #ccc';
 			animate(oNavBox,{height:202},true,function(){
 				oNavBox.style.overflow = 'visible';
 			})
+			//鼠标放上去时加载数据
+			loadData(this.index);
 		}
-		aNavLi[i].onmouseleave = function(){
-			hiddenTimer = setTimeout(hiddenContent,500)
-		}
+		//鼠标移除事件
+		aNavLi[i].onmouseleave = hiddenContent;
 	}
 	oNavBox.onmouseenter = function(){
 		clearTimeout(hiddenTimer);
 	}
-	oNavBox.onmouseleave = function(){
-		hiddenContent();
-	}
+	oNavBox.onmouseleave = hiddenContent;
 	function hiddenContent(){
+		hiddenTimer = setInterval(function(){
 			oNavBox.style.overflow = 'hidden';
 			animate(oNavBox,{height:0},true,function(){
 				oNavBox.style.borderTop = 'none';
 			})
+		}, 500)
+	}
+	function loadData(index){
+		var data = navData[index];
+		/*
+		<!-- 	<ul class="children-list">
+					<li class="children-list-item">
+						<div class="img-box">
+							<a href="#">
+								<img src="images/yuntai.jpg" alt="">
+							</a>
+						</div>
+						<p class="product-title">小米米家智能摄像机云台版</p>
+						<p class="product-price">199元起</p>
+						<sapn class="flag">热卖</sapn>
+					</li>
+					*/
+		var html = 	"<ul class="children-list">";
+		html += "<li class="children-list-item">";			
+		html += "<div class="img-box">"	;			
+		html += "<a href="+data[i].url+">";					
+		html += "<img src="+data[i].img+" alt="">";						
+		html += "</a>"	;				
+		html += "</div>";				
+		html += "<p class="product-title">"+data[i].title+"</p>";				
+		html += "<p class="product-price">"+data[i].price+"</p>";	
+		if(data[i].flag){
+			html += "<sapn class="flag">"+data[i].flag+"</sapn>";
+		}			
+		html += "</li>"	;
+		html += "</ul>"	;
+
 	}
 
 }
@@ -160,11 +193,11 @@ function handleFlashTimer(){
 	setInterval(handlerTimer,500);
 	handlerTimer();
 }
-handleFlashProduct();
-function handleFlashProduct(){
-	var leftBtn = document.querySelector('more-but more-but-left');
-	var rightBtn = document.querySelector('more-but more-but-right');
-	leftBtn.onclick = function(){
+// handleFlashProduct();
+// function handleFlashProduct(){
+// 	var leftBtn = document.querySelector('more-but more-but-left');
+// 	var rightBtn = document.querySelector('more-but more-but-right');
+// 	leftBtn.onclick = function(){
 		
-	}
-}		
+// 	}
+// }		
