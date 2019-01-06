2ae5f26cf0137bbdd85ea8829cd9e4346c729e30
Jsen <1521300387@qq.com>
2019-01-06 20:43:30 +0800

    learn server for background program

---
+++ wl2018/05-javascript/044-serverGET.js
@@ -0,0 +1,26 @@
+//处理GET请求
+var http = require('http');
+var  fs = require('fs');
+var hostname = '127.0.0.1';
+var port = 3000;
+
+var server = http.createServer(function(req,res){
+	console.log(req.url);
+	var urlStr = req.url;
+	if(urlStr == '/favicon.ico'){
+		res.end('/favicon.ico');
+	}
+	var filePath = './' + urlStr;
+	fs.readFile(filePath,function(err,data){
+		if(!err){
+			res.end(data);
+		}else{
+			res.statusCode = 404;
+			res.end('not found....');
+		}
+	})
+})
+
+server.listen(port,hostname,function(){
+	console.log('Server runing at http://'+hostname+':'+port+'/');
+})
