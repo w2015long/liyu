7ae4752f8580e4919956694d892133ba6753a9fe
wl <w2015long@qq.com>
2019-01-15 16:11:36 +0800

    add animate for band carousel

--- mi-band/js/band.js
+++ mi-band/js/band.js
@@ -1,4 +1,7 @@
-
+//封装一个获取元素ID的函数
+function byId(id){
+    return typeof(id) === "string"?document.getElementById(id):id;
+}
 /*购物车*/
 (function(){
     var oCart = document.querySelector('.top .cart');
@@ -29,17 +32,24 @@
 //固定导航栏
 (function(){
     var isShow = false;
+    var isCenter = false;
+
     window.onscroll=function(){
         var topScroll =document.body.scrollTop||
-                        document.documentElement.scrollTop;
-        var oMain=document.getElementById('main');
-        //滚动的距离,距离顶部的距离
-        if(topScroll>200&&topScroll<700){
-            oMain.style.position = 'fixed';
-            oMain.style.top = '1px';
-            oMain.style.zIndex = '9988';
+                       document.documentElement.scrollTop;
+        var oMain = byId(main);
+        if(topScroll>150&&topScroll<260){
+            if(!isCenter){
+                // oMain.style. = ''
+                animate(oMain,{top:80},false);
+                isCenter = true;
+            }
+
         }else{
-            oMain.style.position = 'static';
+            if(isCenter){
+                animate(oMain,{top:-10},false);
+                isCenter = false;
+            }
         }
 
 
@@ -181,89 +191,69 @@ function handleCategory(){
 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
+handleCarousel();
 //carousel轮播图
-//封装一个获取元素ID的函数
-function byId(id){
-    return typeof(id) === "string"?document.getElementById(id):id;
-}
-var oMain=byId('main');
-var oLeft=byId('left-arrow');
-var oRight=byId('right-arrow');
-var aImg=document.querySelectorAll('#imgs');
-var aFollow=document.querySelectorAll('#follow');
-var now=0;
-var timer=null;
-//改变轮播图
-function changesImg(){
+function handleCarousel(){
+    var oMain=byId('main');
+    var oLeft=byId('left-arrow');
+    var oRight=byId('right-arrow');
+    var aImg=document.querySelectorAll('#imgs');
+    var aFollow=document.querySelectorAll('#follow');
+    var now=0;
+    var timer=null;
+    //改变轮播图
+    function changesImg(){
+        for(var i=0;i<aImg.length;i++){
+           aImg[i].style.zIndex='0';
+           aImg[i].style.opacity='0.2';
+           aFollow[i].className=''; 
+        }
+        aImg[now].style.zIndex='89';
+        aImg[now].style.opacity='1';
+        aFollow[now].className='active';
+    }
+    //右边按钮添加事件
+    oRight.onclick=function(){
+        now++
+        if(now>=aImg.length){
+            now=0;
+        }
+        changesImg();
+    }
+    //左边按钮事件
+    oLeft.onclick=function(){
+        now--
+        if(now<0){
+            now=aImg.length-1;
+        }
+        changesImg();
+    }
+    //点击follow切换图片
     for(var i=0;i<aImg.length;i++){
-       aImg[i].style.zIndex='0';
-       aImg[i].style.opacity='0.2';
-       aFollow[i].className=''; 
+        aFollow[i].index=i;//给每一个aFollow绑定一个事件
+        aFollow.onclick=function(){
+            now=this.index;
+            changesImg(); 
+        }
     }
-    aImg[now].style.zIndex='89';
-    aImg[now].style.opacity='1';
-    aFollow[now].className='active';
-}
-//右边按钮添加事件
-oRight.onclick=function(){
-    now++
-    if(now>=aImg.length){
-        now=0;
+    //自动播放轮播图
+    function startAutoPlay(){
+        timer=setInterval(oRight.onclick,2000);
     }
-    changesImg();
-}
-//左边按钮事件
-oLeft.onclick=function(){
-    now--
-    if(now<0){
-        now=aImg.length-1;
-    }
-    changesImg();
-}
-//点击follow切换图片
-for(var i=0;i<aImg.length;i++){
-    aFollow[i].index=i;//给每一个aFollow绑定一个事件
-    aFollow.onclick=function(){
-        now=this.index;
-        changesImg(); 
-    }
-}
-//自动播放轮播图
-function startAutoPlay(){
-    timer=setInterval(oRight.onclick,1000);
-}
-startAutoPlay();
-//暂停播放
-function stopAutoPlay(){
-    if(timer){
-        clearInterval(timer);
-    }
-}
-//鼠标离开main区自动播放
-oMain.onmouseout=function(){
     startAutoPlay();
-}
-//鼠标放到main区停止播放
-oMain.onmouseover=function(){
-    stopAutoPlay();
-}
-*/
+    //暂停播放
+    function stopAutoPlay(){
+        if(timer){
+            clearInterval(timer);
+        }
+    }
+    //鼠标离开main区自动播放
+    oMain.onmouseout=function(){
+        startAutoPlay();
+    }
+    //鼠标放到main区停止播放
+    oMain.onmouseover=function(){
+        stopAutoPlay();
+    }
+}    
+
