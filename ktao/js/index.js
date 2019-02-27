4866a04b91a876735a43478f99ceab760acaee02
wl <w2015long@qq.com>
2019-02-27 11:52:50 +0800

    add ktao top jq

---
+++ ktao/js/index.js
@@ -0,0 +1,11 @@
+;(function($){
+	$('.dropdown').hover(function(){
+		var $this = $(this);
+		var activeClass = $this.data('active') + '-active' ;  
+		$this.addClass(activeClass);
+	},function(){
+		var $this = $(this);
+		var activeClass = $this.data('active') + '-active' ; 
+		$this.removeClass(activeClass);
+	})
+})(jQuery);
