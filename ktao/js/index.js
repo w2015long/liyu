8e24d74b7a7db39b3292a6986ad9d3d54aa367ab
Jsen <w2015long@qq.com>
2019-02-28 22:15:09 +0800

    add loading for dropdown

--- ktao/js/index.js
+++ ktao/js/index.js
@@ -1,13 +1,17 @@
 ;(function($){
-	var $menuDrop = $('.dropdown')
+	var $menuDrop = $('.dropdown');
+	var $layer = $('.dropdown-layer');
 	$menuDrop.dropdown({js:true,mode:'slideDownUp'});
-
 	$menuDrop.on('dropdown-show',function(ev){
-		var loadUrl = $menuDrop.data('file');
+		var $elem = $(this);
+		var loadUrl = $elem.data('file');
 		if(!loadUrl) return;
 		$.getJSON(loadUrl, function(data) {
-				/*optional stuff to do after success */
-				console.log(data);
+				var html = '';
+				for(var i=0;i<data.length;i++){
+					html += '<li class="dropdown-item"><a href="'+data[i].url+'">'+data[i].name+'</a></li>'
+				}
+				$layer.html(html);
 		});
 	});
 
