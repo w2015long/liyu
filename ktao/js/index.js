2e56c94f706225c8f7b8925db7b1672426592b29
wl <w2015long@qq.com>
2019-03-01 11:43:35 +0800

    add set css

--- ktao/js/index.js
+++ ktao/js/index.js
@@ -1,19 +1,31 @@
 ;(function($){
 	var $menuDrop = $('.dropdown');
-	var $layer = $('.dropdown-layer');
+	
 	$menuDrop.dropdown({js:true,mode:'slideDownUp'});
 	$menuDrop.on('dropdown-show',function(ev){
 		var $elem = $(this);
 		var loadUrl = $elem.data('file');
 		if(!loadUrl) return;
+		var $layer = $elem.find('.dropdown-layer');
+		//防止多次请求数据 设置一个变量控制是否请求
+		var isLoaded = $elem.data('isLoaded');
+		if(isLoaded) return ;//如果已经请求到数据 就终止
 		$.getJSON(loadUrl, function(data) {
 				var html = '';
 				for(var i=0;i<data.length;i++){
 					html += '<li class="dropdown-item"><a href="'+data[i].url+'">'+data[i].name+'</a></li>'
 				}
-				$layer.html(html);
+				//模拟网络延时
+				setTimeout(function(){
+					$layer.html(html);
+					//第一次请求到数据 变量设为true
+					$elem.data('isLoaded',true);
+				},800);
+				
 		});
 	});
 
-
+	//search
+	var $search = $('.main-search');
+	$search.search();
 })(jQuery);
--- ktao/js/search.js
+++ ktao/js/search.js
@@ -3,13 +3,27 @@
 		//1.罗列属性
 		this.$elem = $elem;
 		this.options = options;
+		this.searchBtn = $elem.find('.search-btn');
+		this.searchForm = $elem.find('.search-form');
+		this.searchInput = $elem.find('.search-keyword');
 		//2.初始化
 		this.init();
 	}
 	Search.prototype = {
 		constructor:Search,
 		init:function(){
-			
+			//1.绑定事件
+			this.searchBtn.on('click',$.proxy(this.submit,this));
+		},
+		submit:function(){
+			if(getInputVal() == ''){
+				//input为空就阻止默认提交
+				return false;
+			}
+			this.searchForm.trigger('submit');
+		},
+		getInputVal:function(){
+			return $.trim(this.searchInput.val());
 		}
 	}
 	Search.DEFAULTS = {
@@ -25,7 +39,7 @@
 				var searchObj = $elem.data('search');
 				if(!searchObj){//单例模式
 					options = $.extend({},Search.DEFAULTS,options);
-					searchObj = new search($elem,options);
+					searchObj = new Search($elem,options);
 					$elem.data('search',searchObj);
 				}
 				if(typeof searchObj[options] == 'function'){
