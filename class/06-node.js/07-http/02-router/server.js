a160f12a36bb8ccb3fb6bd1a2dc1b8392ab8c44f
Jsen <w2015long@qq.com>
2019-03-22 22:51:51 +0800

    add router

--- class/06-node.js/07-http/02-router/server.js
+++ class/06-node.js/07-http/02-router/server.js
@@ -1,12 +1,49 @@
 const http = require('http');
+const fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req,res)=>{
-  	console.log('res.url=>',req.url)
+	//req=request 可读流
+	//res=response 可写流
+  	console.log('res.url=>',req.url);
   	const filePath = req.url;
-  	res.setHeader('Content-Type', "text/html; charset=utf-8");	
-	res.write('hello 你好')
-	res.end();
+  	switch(filePath){
+  		case '/index.html':
+	  		fs.readFile('./index.html',(err,data)=>{
+	  			if(err) throw err;
+	  			res.setHeader('Content-Type', "text/html; charset=utf-8");	
+	  			res.end(data);
+  			}) 
+  			break;
+  		case '/css/index.css':	
+	  		fs.readFile('./css/index.css',(err,data)=>{
+	  			if(err) throw err;
+	  			res.setHeader('Content-Type', "text/css; charset=utf-8");	
+	  			res.end(data);
+  			})
+  			break;  
+  		case '/js/index.js':	
+	  		fs.readFile('./js/index.js',(err,data)=>{
+	  			if(err) throw err;
+	  			res.setHeader('Content-Type', "application/x-javascript; charset=utf-8");	
+	  			res.end(data);
+  			})
+  			break; 
+  		case '/images/first.jpg':	
+	  		fs.readFile('./images/first.jpg',(err,data)=>{
+	  			if(err) throw err;
+	  			res.setHeader('Content-Type', "image/jpeg; charset=utf-8");	
+	  			res.end(data);
+  			})
+  			break; 
+  		default:
+  			res.setHeader('Content-Type', "text/html; charset=utf-8");
+  			res.statusCode = 404;
+  			res.end('<h1>页面找不到</h1>');
+  			break;	 			  			  				
+  	}
+
+  	
 })
 
 server.listen(port, hostname, () => {
