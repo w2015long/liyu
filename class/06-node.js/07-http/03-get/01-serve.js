2ee8b24e7d88c7b9b331f2b54c58efc56737493a
Jsen <w2015long@qq.com>
2019-03-23 22:53:55 +0800

    add get url query

--- class/06-node.js/07-http/03-get/01-serve.js
+++ class/06-node.js/07-http/03-get/01-serve.js
@@ -1,11 +1,27 @@
+
 const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+
 const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req,res)=>{
-  	res.statusCode = 200;
+  	// res.statusCode = 200;
+  	/*
+  //url.parse(urlString[, parseQueryString[, slashesDenoteHost]])
+  	const objUrl = url.parse(req.url);
+  	console.log("objUrl=>",objUrl);
+  	const queryStr = objUrl.query;
+  	console.log("objUrl.query=>",queryStr);
+//querystring.parse(str[, sep[, eq[, options]]])
+	const queryObj = querystring.parse(queryStr);
+	console.log('queryObj',queryObj);
+	*/
+	const objUrl = url.parse(req.url,true);
+	console.log('objUrl=>',objUrl);
+	console.log('objUrl.query=>',objUrl.query)
   	res.setHeader('Content-Type','text/html; charset=utf-8');	
-	res.write('hello 你好')
-	res.end();
+	res.end('index is skip');
 })
 
 server.listen(port, hostname, () => {
