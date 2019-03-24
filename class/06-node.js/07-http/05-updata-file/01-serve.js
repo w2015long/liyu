1874758e7ab35b515a4c5bb6375325cf10dbc23d
dragon <w2015long@qq.com>
2019-03-24 10:08:56 +0800

    learn formidable

--- class/06-node.js/07-http/05-updata-file/01-serve.js
+++ class/06-node.js/07-http/05-updata-file/01-serve.js
@@ -1,11 +1,25 @@
 const http = require('http');
 const querystring = require('querystring');
+const formidable = require('formidable');
+
+
 const hostname = '127.0.0.1';
 const port = 3000;
 const server = http.createServer((req,res)=>{
   	//console.log(`req.url=>${req.url}\nreq.method=>${req.method}`);
+    let form = new formidable.IncomingForm();
+    form.uploadDir = "./upload/";
+    form.keepExtensions  = true;
+    form.parse(req, function(err, fields, files) {
+        
+        console.log({fields: fields, files: files});
+        res.setHeader('Content-Type','text/html; charset=utf-8');
+        res.end('end')        
 
-  	res.setHeader('Content-Type','text/html; charset=utf-8');	
+    });
+
+  	
+    /*	
   	let body = '';
   	req.on('data',chunk=>{
   		body += chunk;
@@ -15,7 +29,8 @@ const server = http.createServer((req,res)=>{
       const obj = querystring.parse(body);
       console.log(obj)
   	})
-    res.end('end')
+    */
+
 })
 
 server.listen(port, hostname, () => {
