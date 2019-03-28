f2282965218829489e46deebfcd1d0216c47d1f7
dragon <w2015long@qq.com>
2019-03-28 16:29:06 +0800

    add driver for mongodb

--- class/07-mongodb/02-driver/app.js
+++ class/07-mongodb/02-driver/app.js
@@ -8,13 +8,45 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'liyu';
 
 // Create a new MongoClient
-const client = new MongoClient(url);
+const client = new MongoClient(url,{ useNewUrlParser: true });
 
 // Use connect method to connect to the Server
 client.connect((err)=> {
   console.log("Connected successfully to server");
 
   const db = client.db(dbName);
+  //Insert a Document
+  const school = db.collection('school');
+  /*
+  school.insertMany(
+  		[
+  			{name:'Anmy',age:33,major:'computer'},
+  			{name:'Leo',age:23,major:'English'}
+  		],
+	  	
+	  	(err,result)=>{
+	  		if(err) return console.log('insertMany err',err);
+	  		console.log(result);
+	  	}
+  )
+  */
+ /*
+  school.find({name:'Leo'}).toArray((err, docs)=>{
+  		if(err) return console.log('find err',err);
+	  	console.log(docs);
+  })
+	
 
+
+  school.updateOne({name:'Leo'},{$set:{name:'Mike'}},(err, result)=>{
+  		if(err) return console.log('updateOne err',err);
+	  	console.log(result);
+  })
+*/
+  school.deleteOne({name:'Anmy'},(err, result)=>{
+  		if(err) return console.log('deleteOne err',err);
+	  	console.log(result);
+  })
   client.close();
+
 });
