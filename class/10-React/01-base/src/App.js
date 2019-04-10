fa2dbfe4a6fb8f4d73b5ec29ccf583ba5d275cd7
dragon <w2015long@qq.com>
2019-04-10 11:54:36 +0800

    add export and from

---
+++ class/10-React/01-base/src/App.js
@@ -0,0 +1,25 @@
+
+/*
+version 1
+const a = 1;
+const b = 2;
+//重命名
+export {a as aNum,b}
+*/
+
+/*
+//version 2
+export const a = 1;
+*/
+
+/*
+//version 3
+const str = 'React';
+export {str};
+*/
+
+//version 4
+const str = 'Hello React';
+
+export default str;
+
--- class/10-React/01-base/src/index.js
+++ class/10-React/01-base/src/index.js
@@ -2,4 +2,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+
+
 ReactDOM.render(<h1>Hello React!</h1>,document.getElementById('root')) 
+
+/*
+//version 1
+import {aNum,b} from './App.js'
+console.log(aNum,b)
+*/
+
+/*
+//version 2
+import {a} from './App.js'
+console.log(a)
+*/
+
+/*
+//version 3
+import {str as message} from './App.js'
+console.log(message)
+*/
+
+
+//version 4
+
+import message from './App.js'
+
+console.log(message)
