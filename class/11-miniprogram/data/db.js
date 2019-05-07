8d0eaf005f7b2b17a1fdd24cedd98b9476614663
dragon <w2015long@qq.com>
2019-05-07 20:58:49 +0800

    handle music

--- class/11-miniprogram/data/db.js
+++ class/11-miniprogram/data/db.js
@@ -1,20 +1,24 @@
 var articles = [
   {
-    avatar: '../../images/blog.jpg',
+    avatar: '/images/blog.jpg',
+    author:'Tom',
     date: '2019-05-06',
-    title: '华为meit30发布会',
-    photo: '../../images/carousel3.jpg',
+    title: '小米发布会',
+    photo: '/images/carousel3.jpg',
     desc: '我是文章，使用警告：此Api非官方版本，请各位尽量将分享功能迁移至腾讯官方版，会更稳定些',
+    content:'我是文章，使用警告：此Api非官方版本，请各位尽量将分享功能迁移至腾讯官方版，会更稳定些我是文章，使用警告：此Api非官方版本，请各位尽量将分享功能迁移至腾讯官方版，会更稳定些我是文章，使用警告：此Api非官方版本，请各位尽量将分享功能迁移至腾讯官方版，会更稳定些',
     star: '9999',
     view: '86520',
     articleId:'0'
   },
   {
-    avatar: '../../images/blog.jpg',
+    avatar: '/images/blog.jpg',
+    author: 'Amy',
     date: '2019-05-09',
-    title: '华为meit56发布会',
-    photo: '../../images/bg4.jpg',
+    title: '演唱会',
+    photo: '/images/bg4.jpg',
     desc: '分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试',
+    content:"分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试分享失败了，是不是可以告诉用户：不要紧，可能是网络问题，一会儿再试试",
     star: '8966',
     view: '5688',
     articleId: '1'
--- class/11-miniprogram/pages/article/article-detail/article-detail.js
+++ class/11-miniprogram/pages/article/article-detail/article-detail.js
@@ -1,3 +1,4 @@
+var { articles } = require('../../../data/db.js')
 Page({
 
   /**
@@ -12,58 +13,52 @@ Page({
    */
   onLoad: function (options) {
     // console.log(options)
-    var articleId = options.articleId
-    console.log(articleId)
+    var articleId = options.articleId;
+    var article = articles[articleId];
+    //处理收藏状态
+    var collect_articel = wx.getStorageSync('collect_articel');
+
+    var isCollected = false; 
+    if (!collect_articel){
+      var data = null;
+      data[articleId] = false;
+      wx.setStorageSync('collect_articel', data)
+    }else{
+      isCollected = !!collect_articel[articleId]
+    }
+
+  
+    this.setData({ ...article,isCollected:isCollected});
+  },
+  //收藏
+  tapCollect:function(){
+    // wx.setStorageSync('key',123)
+    var collect_articel = wx.getStorageSync('collect_articel');
+    var isCollected = collect_articel[this.data[articleId]];
+    //改变storage里面的数据
+    collect_articel[this.data[articleId]] = !isCollected;
+    wx.setStorageSync('collect_articel', collect_articel);
+    ////改变视图页面
+    this.setData({
+      isCollected:isCollected
+    },function(){
+
+    }.bind(this))
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 处理分享
    */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  tapShare:function(){
+    var list = ['分享到朋友圈','分享到微博','分享到QQ']
+    wx.showActionSheet({
+      itemList: list ,
+      success(res){
+        wx.showToast({
+          title:list[res.tapIndex]+'成功'
+        })
+      }
+    })
   }
-
 
 })
