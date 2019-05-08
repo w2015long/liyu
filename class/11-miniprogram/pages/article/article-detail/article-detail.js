fab0c0dbbdcec5d6625fc27b291d1ebda059a7ce
dragon <w2015long@qq.com>
2019-05-08 21:03:54 +0800

    add movie template

--- class/11-miniprogram/pages/article/article-detail/article-detail.js
+++ class/11-miniprogram/pages/article/article-detail/article-detail.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    isPlaying:false
   },
 
   /**
@@ -17,32 +17,49 @@ Page({
     var article = articles[articleId];
     //处理收藏状态
     var collect_articel = wx.getStorageSync('collect_articel');
-
+    //缓存对应的id 到data中 
+    this.data[articleId] = articleId;
+    /**
+     * "0":false,
+     * "1":true
+     */
     var isCollected = false; 
     if (!collect_articel){
-      var data = null;
-      data[articleId] = false;
-      wx.setStorageSync('collect_articel', data)
+      this.data[articleId] = false;
+      wx.setStorageSync('collect_articel', this.data)
     }else{
       isCollected = !!collect_articel[articleId]
     }
-
+    //监听音乐相关事件
+    var BackgroundAudioManager = wx.getBackgroundAudioManager();
+    BackgroundAudioManager.onPlay(function(){
+      this.setData({
+        isPlaying: true
+      })
+    }.bind(this));
+    BackgroundAudioManager.onPause(function(){
+      this.setData({
+        isPlaying: false
+      })
+    }.bind(this))
   
-    this.setData({ ...article,isCollected:isCollected});
+    this.setData({ ...article, isCollected: isCollected});
   },
   //收藏
   tapCollect:function(){
-    // wx.setStorageSync('key',123)
+    
     var collect_articel = wx.getStorageSync('collect_articel');
-    var isCollected = collect_articel[this.data[articleId]];
+    var isCollected = collect_articel[this.data.articleId];
     //改变storage里面的数据
-    collect_articel[this.data[articleId]] = !isCollected;
+    collect_articel[this.data.articleId] = !isCollected;
     wx.setStorageSync('collect_articel', collect_articel);
     ////改变视图页面
     this.setData({
-      isCollected:isCollected
+      isCollected:!isCollected
     },function(){
-
+      wx.showToast({
+        title: isCollected　? '取消收藏' : '收藏成功' ,
+      })
     }.bind(this))
   },
 
@@ -59,6 +76,31 @@ Page({
         })
       }
     })
-  }
+  },
+  /**
+   * 处理背景音乐
+   */
+  tapMusic:function(){
+  
+    var BackgroundAudioManager = wx.getBackgroundAudioManager();
 
+    var isPlaying = this.data.isPlaying;
+    if(isPlaying){
+      BackgroundAudioManager.pause();
+      this.setData({
+        isPlaying:false
+      })
+    }else{
+      var music = articles[this.data.articleId].music;
+     
+      BackgroundAudioManager.src = music.src;
+      BackgroundAudioManager.title = music.title;
+      BackgroundAudioManager.coverImgUrl = music.cover;
+
+      // BackgroundAudioManager.play();
+      this.setData({
+        isPlaying: true
+      })    
+    }
+  }
 })
---
+++ class/11-miniprogram/pages/movie/movie.js
@@ -0,0 +1,24 @@
+// pages/movie/movie.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    /*
+    wx.request({
+      // url:'https://api.douban.com/v2/movie/top250',
+      success:function(res){
+        console.log(res)
+      }
+    })
+    */
+  }
+})
