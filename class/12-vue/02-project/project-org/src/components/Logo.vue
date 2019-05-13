2784ba180ec025b17c225c06dc78e27b7345a59c
dragon <w2015long@qq.com>
2019-05-13 21:02:39 +0800

    todolist

---
+++ class/12-vue/02-project/project-org/src/components/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="logo">
+		<img src="../assets/logo.png" alt="">
+	</div>
+</template>
+<script type="text/javascript">
+	export default {
+		name:'Logo'
+	}
+</script>
+<style type="text/css">
+	.logo img{
+		width: 300px;
+	}
+</style>
